bc1a432f969a234dcd754eb4d45ea7bf07483377
chrper80 <chrper80@gmail.com>
2020-12-19 18:45:31 +0000

    Added functionality, content and styling to the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,31 @@ $(".hamburgerIcon").click(function(){
     $("nav").toggleClass("hide");
 });
 
-$("nav li").click(function(){
-    $(this).toggleClass("addBackgroundColorOnClick")
-});
-
 $("#toTopButton").click(function(){
     document.getElementById("header").scrollIntoView(true);
+});
+
+$(".read-more1").click(function(){
+    $(".card-article1").toggle("fast");
+});
+
+$(".read-more2").click(function(){
+    $(".card-article2").toggle("fast");
+});
+
+$(".read-more3").click(function(){
+    $(".card-article3").toggle("fast");
+});
+
+$(".read-more4").click(function(){
+    $(".card-article4").toggle("fast");
+});
+
+$(".read-more5").click(function(){
+    $(".card-article5").toggle("fast");
+});
+
+$(".read-more6").click(function(){
+    $(".card-article6").toggle("fast");
 });
 
