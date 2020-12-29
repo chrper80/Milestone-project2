d3cea666f57783f535e11f28c8d2a55c7970c0a1
chrper80 <chrper80@gmail.com>
2020-12-29 22:19:53 +0000

    Added a popup when sending message, also started adding some interactivity to the about page

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,14 +1,24 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
+document.getElementById('contact-form').addEventListener('submit', function (event) {
     event.preventDefault();
     emailjs.sendForm('contact_service', 'contact_form', this)
-        .then(function() {
-            console.log('SUCCESS!');
-        }, function(error) {
-            console.log('FAILED...', error);
+        .then(function () {
+            Swal.fire(
+                'Sent!',
+                'Your message was sent.',
+                'success'
+            )
+        }, function () {
+            Swal.fire(
+                'Error',
+                'An error occurred',
+                'error'
+            )
         })
-        .then(function() {
-            document.getElementById('contact-form').reset(); 
+        .then(function () {
+            document.getElementById('contact-form').reset();
         })
 })
+
+
 
 
