6cd3ca60ebccfdb8d16c8e9f6712e38413e7aee0
chrper80 <chrper80@gmail.com>
2020-12-31 22:18:13 +0000

    Perfected comments, added  a logo on the mobile version, added interactivity on the about page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,20 @@
-/*Function that makes the mobile menu visible when clicking the hamburger icon*/
+/*Mobile menu*/
+/*Makes it visible and hides it*/
 function hamburgerMenu() {
     let a = document.querySelector("#mobile-menu");
     a.classList.toggle("hide");
 }
 
-/*Function that brings the user back to top of page when clicking the up-arrow button*/
+/*Footer*/
+/*Brings user back to top when clicking up-arrow*/
+/*The tutor team gave me this one*/
 function backToTop() {
     let a = document.querySelector("#header");
     a.scrollIntoView(true);
 }
 
-/*Functions that changes the color of the text when hovering the menu and footer items*/
+/*Menu and footer*/
+/*Changes color on hover*/
 $("#header-large-screens a").hover(function () {
     $(this).toggleClass("change-color-on-hover");
 });
@@ -20,18 +24,20 @@ $("#footer i").hover(function () {
     $(this).toggleClass("change-color-on-hover");
 });
 
-/*Function that changes the background-color when hovering over buttons*/
+/*Buttons*/
+/*Changes bg color on hover*/
 function changeBackgroundColorOfButtons(a) {
     $(a).toggleClass("background-readmore");
 }
 
-
-/*Function that reveals info on workplace-cards and testimonials*/
+/*Workplaces*/
+/*Reveals info on cards when clicking button*/
 function readMore(a) {
     $(a).toggle("fast");
 }
 
-/*Function that changes the text of the "Read more" to "Show less" in the workplaces section*/
+/*Workplaces and Testimonials*/
+/*Changes the text of the "Read more" to "Show less"*/
 function changeText(a) {
     let toggleText = document.querySelector(a);
     if (toggleText.innerHTML == "Read More") {
@@ -53,14 +59,44 @@ function initMap() {
     new google.maps.Marker({
         position: myWorkPlace,
         map,
-        title: "Norra Sjöbogatan 34, 50643 Borås. That´s my home",
+        title: "Norra Sjöbogatan 34, 50643 Borås",
     });
 }
 
-/*Reveals hidden text in the testimonial-cards*/
+/*Testimonials*/
+/*Reveals hidden text when clicking button*/
 function slideToggle(a) {
     $(a).slideToggle("fast");
 }
+
+/*About*/
+/*Changes bg-color and some other properties when hovering over items*/
+function highlightAbout(a) {
+    b = document.querySelector(a);
+    b.classList.toggle("change-bg-color-about");
+}
+
+/*About*/
+/*Reduces opacity on the other items when hovering over them*/
+function reduceOpacity(a, b, c, d, e, f) {
+    $(a).addClass("reduce-opacity-about");
+    $(b).addClass("reduce-opacity-about");
+    $(c).addClass("reduce-opacity-about");
+    $(d).addClass("reduce-opacity-about");
+    $(e).addClass("reduce-opacity-about");
+    $(f).addClass("reduce-opacity-about");
+}
+
+/*Removes the class above*/
+function resetOpacity(a, b, c, d, e, f) {
+    $(a).removeClass("reduce-opacity-about");
+    $(b).removeClass("reduce-opacity-about");
+    $(c).removeClass("reduce-opacity-about");
+    $(d).removeClass("reduce-opacity-about");
+    $(e).removeClass("reduce-opacity-about");
+    $(f).removeClass("reduce-opacity-about");
+}
+
 
 
 
--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,23 +1,29 @@
+
+/*I had a lot of help from the tutor team and my mentor with the code in this file*/
+
 document.getElementById('contact-form').addEventListener('submit', function (event) {
     event.preventDefault();
     emailjs.sendForm('contact_service', 'contact_form', this)
         .then(function () {
-            Swal.fire(
+            /*Calling Sweet alert with Swal.fire. My mentor helped me here*/
+            Swal.fire( 
                 'Sent!',
                 'Your message was sent.',
                 'success'
-            )
+            );
         }, function () {
             Swal.fire(
                 'Error',
                 'An error occurred',
                 'error'
-            )
+            );
         })
+        
+        /*The tutor team helped me here*/
         .then(function () {
             document.getElementById('contact-form').reset();
-        })
-})
+        });
+});
 
 
 
